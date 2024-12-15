e296c8362d6090b798da4ed0a6655f573366ad12
Jose Mateus <jmateussilva044@gmail.com>
2024-12-15 09:46:26 -0300

    formulario do blog

--- articles/articles_controller.js
+++ articles/articles_controller.js
@@ -5,4 +5,8 @@ router.get("/articles", (request, response) => {
     response.send("rota de artigos.")
 })
 
+router.get("/admin/articles/new", (request, response) => {
+    response.render("admin/articles/new")
+})
+
 module.exports = router
