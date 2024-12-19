d64e5fcc8ab1cf30b3167caea3aa01413a4c1445
Jose Mateus <jmateussilva044@gmail.com>
2024-12-18 21:57:51 -0300

    esta atualizando o artigo

--- articles/articles_controller.js
+++ articles/articles_controller.js
@@ -34,4 +34,61 @@ router.post("/article/save", (request, response) => {
     })
 })
 
+router.post("/articles/delete", (request, response) => {
+    var id = request.body.id 
+    if(id != undefined)
+    {
+        if(!isNaN(id))
+        {   
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                response.redirect("/admin/articles")
+            })
+        }
+        else response.redirect("/admin/articles")
+    }
+    else response.redirect("/admin/articles")
+})
+
+
+router.get("/admin/articles/edit/:id", (request, response) => {
+    var id = request.params.id;
+    if (isNaN(id)) return response.redirect("/admin/articles");
+
+    Article.findByPk(id).then(article => {
+        if (article) {
+            Category.findAll().then(categories => {
+                response.render("admin/articles/edit_article", { 
+                    article: article, 
+                    categories: categories 
+                })
+            })
+        } else {
+            response.redirect("/admin/articles");
+        }
+    }).catch(error => {
+        response.redirect("/admin/articles");
+    })
+})
+
+
+
+router.post("/admin/article/update", (request, response) => {
+    var id = request.body.id
+    var title = request.body.title
+    var body = request.body.body
+    var categoryId = request.body.category;
+    Article.update({title: title, slug: slugify(title), body: body, categoryId: categoryId}, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        response.redirect("/admin/articles")
+    })
+})
+
+
 module.exports = router
